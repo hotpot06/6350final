2fd69a61571ff0171a8e54b3ade86f2b5cab1e21
jie-liu <106674449+hotpot06@users.noreply.github.com>
2023-12-09 18:11:19 -0800

    Add files via upload

---
+++ StockAPIExample/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  StockAPIExample
+//
+//  Created by Hotpot Liu on 12/9/23.
+//
+
+import Foundation
+
+let baseURL = "https://us-central1-fir-api-s-8d31b.cloudfunctions.net/app"
+let apiKey = "AIzaSyArxUeTxwV_PTXzO7b3pKpGjVg3k2kCDmM"
+
---
+++ StockAPIExample/WeatherClass.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherClass.swift
+//  StockAPIExample
+//
+//  Created by Hotpot Liu on 12/9/23.
+//
+
+import Foundation
+class WeatherClass {
+    var cityCode: String = ""
+    var city: String = ""
+    var temperature: Int = 0
+    var conditions: String = ""
+}
