433a7cc46258f5e348244aca36eca0533a2935e3
jie-liu <106674449+hotpot06@users.noreply.github.com>
2023-12-09 18:08:54 -0800

    Add files via upload

---
+++ NamePhoneNumberProtocolDelegate/NamePhoneNumberProtocolDelegate/NamePhoneNumberViewController.swift
@@ -0,0 +1,32 @@
+//
+//  NamePhoneNumberViewController.swift
+//  NamePhoneNumberProtocolDelegate
+//
+//  Created by Hotpot Liu on 12/9/23.
+//
+
+import UIKit
+
+class NamePhoneNumberViewController: UIViewController {
+
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var name: UITextField!
+    
+    var sendNamePhoneDelegate: SendNamePhoneNumberDelegate?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveNamePhone(_ sender: Any) {
+        guard let name = name.text else {return}
+        guard let phoneNumber = phoneNumber.text else {return}
+        
+        sendNamePhoneDelegate?.sendNamePhoneNumber(name: name, phoneNumber: phoneNumber)
+        
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+}
---
+++ NamePhoneNumberProtocolDelegate/NamePhoneNumberProtocolDelegate/SendNamePhoneNumberDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SendNamePhoneNumberDelegate.swift
+//  NamePhoneNumberProtocolDelegate
+//
+//  Created by Hotpot Liu on 12/9/23.
+//
+
+import Foundation
+
+protocol SendNamePhoneNumberDelegate{
+    func sendNamePhoneNumber(name: String, phoneNumber: String)
+}
